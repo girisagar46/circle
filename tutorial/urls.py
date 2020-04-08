9cf7bec71c75dfa67806ec0b5d33f2e2ba3f8c5d
sagar.giri <sagar.giri@bebit.com>
2020-04-08 21:59:57 +0900

    ok

--- tutorial/urls.py
+++ tutorial/urls.py
@@ -11,7 +11,7 @@ router.register(r'groups', views.GroupViewSet)
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    url(r'^$', include(router.urls)),
+    url(r'', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^admin/', admin.site.urls),
 ]
